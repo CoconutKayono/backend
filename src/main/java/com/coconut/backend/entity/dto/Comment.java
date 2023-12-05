2424cd9da97bad06f7105ddcaf103ffc5aa6e1f9
coconut <test>
2023-12-05 19:49:00 +0800

    优化代码

--- src/main/java/com/coconut/backend/entity/dto/Comment.java
+++ src/main/java/com/coconut/backend/entity/dto/Comment.java
@@ -34,4 +34,63 @@ public class Comment implements Serializable {
         this.setSupport(getSupport() - 1);
     }
 
+    public static class Builder {
+        private Integer id;
+        private Integer userId;
+        private Integer noteId;
+        private Integer parentId;
+        private String comment;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime createdTime;
+        private Integer support;
+
+        public Builder id(Integer val) {
+            id = val;
+            return this;
+        }
+
+        public Builder userId(Integer val) {
+            userId = val;
+            return this;
+        }
+
+        public Builder noteId(Integer val) {
+            noteId = val;
+            return this;
+        }
+
+        public Builder parentId(Integer val) {
+            parentId = val;
+            return this;
+        }
+
+        public Builder comment(String val) {
+            comment = val;
+            return this;
+        }
+
+        public Builder createdTime(LocalDateTime val) {
+            createdTime = val;
+            return this;
+        }
+
+        public Builder support(Integer val) {
+            support = val;
+            return this;
+        }
+
+        public Comment build() {
+            return new Comment(this);
+        }
+    }
+
+    private Comment(Builder builder) {
+        this.id = builder.id;
+        this.userId = builder.userId;
+        this.noteId = builder.noteId;
+        this.parentId = builder.parentId;
+        this.comment = builder.comment;
+        this.createdTime = builder.createdTime;
+        this.support = builder.support;
+    }
 }
