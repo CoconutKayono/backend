9ee79142d3f1edeeb80295ab20e71683f4a611ae
coconut <test>
2023-12-06 10:15:58 +0800

    优化代码

--- src/main/java/com/coconut/backend/controller/CommentController.java
+++ src/main/java/com/coconut/backend/controller/CommentController.java
@@ -1,10 +1,33 @@
 package com.coconut.backend.controller;
 
+import com.coconut.backend.entity.RestBean;
+import com.coconut.backend.entity.vo.response.CommentVO;
+import com.coconut.backend.entity.vo.response.NoteVO;
+import com.coconut.backend.service.CommentService;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/comment")
 public class CommentController {
+    @Resource
+    CommentService commentService;
+    @GetMapping("/list")
+    public RestBean<List<CommentVO>> list(HttpServletRequest request){
+        Integer id = (Integer) request.getAttribute("id");
+        List<CommentVO> commentVOs;
+        if (id == null) {
+            commentVOs = commentService.listCommentVOs();
+        } else {
+            commentVOs = commentService.listCommentVOs(id);
+        }
+        if (commentVOs != null) return RestBean.success(commentVOs);
+        else return RestBean.failure(404, "暂无任何评论");
+    }
 }
--- src/main/java/com/coconut/backend/service/CommentService.java
+++ src/main/java/com/coconut/backend/service/CommentService.java
@@ -2,8 +2,12 @@ package com.coconut.backend.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.coconut.backend.entity.dto.Comment;
+import com.coconut.backend.entity.vo.response.CommentVO;
+
+import java.util.List;
 
 
 public interface CommentService extends IService<Comment> {
-
+    List<CommentVO> listCommentVOs();
+    List<CommentVO> listCommentVOs(Integer id);
 }
