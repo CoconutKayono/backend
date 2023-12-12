264cee62ab42081052c95dde2c0869386924aac7
coconut <test>
2023-12-12 15:21:40 +0800

    优化代码

--- src/main/java/com/coconut/backend/entity/dto/Message.java
+++ src/main/java/com/coconut/backend/entity/dto/Message.java
@@ -3,12 +3,13 @@ package com.coconut.backend.entity.dto;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -20,124 +21,52 @@ public class Message implements Serializable {
     private Integer senderId;
     private Integer recipientId;
     private String message;
-    private Date createTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
 
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * 
-     */
-    public Integer getSenderId() {
-        return senderId;
+    private Message(Builder builder) {
+        this.id = builder.id;
+        this.senderId = builder.senderId;
+        this.recipientId = builder.recipientId;
+        this.message = builder.message;
+        this.createTime = builder.createTime;
     }
 
-    /**
-     * 
-     */
-    public void setSenderId(Integer senderId) {
-        this.senderId = senderId;
-    }
+    public static class Builder {
+        private Integer id;
+        private Integer senderId;
+        private Integer recipientId;
+        private String message;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime createTime;
 
-    /**
-     * 
-     */
-    public Integer getId() {
-        return id;
-    }
-
-    /**
-     * 
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
-     * 
-     */
-    public Integer getRecipientId() {
-        return recipientId;
-    }
-
-    /**
-     * 
-     */
-    public void setRecipientId(Integer recipientId) {
-        this.recipientId = recipientId;
-    }
-
-    /**
-     * 
-     */
-    public String getMessage() {
-        return message;
-    }
-
-    /**
-     * 
-     */
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    /**
-     * 
-     */
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    /**
-     * 
-     */
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
+        public Builder id(Integer val) {
+            id = val;
+            return this;
         }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        Message other = (Message) that;
-        return (this.getSenderId() == null ? other.getSenderId() == null : this.getSenderId().equals(other.getSenderId()))
-            && (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-            && (this.getRecipientId() == null ? other.getRecipientId() == null : this.getRecipientId().equals(other.getRecipientId()))
-            && (this.getMessage() == null ? other.getMessage() == null : this.getMessage().equals(other.getMessage()))
-            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()));
-    }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getSenderId() == null) ? 0 : getSenderId().hashCode());
-        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
-        result = prime * result + ((getRecipientId() == null) ? 0 : getRecipientId().hashCode());
-        result = prime * result + ((getMessage() == null) ? 0 : getMessage().hashCode());
-        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
-        return result;
-    }
+        public Builder senderId(Integer val) {
+            senderId = val;
+            return this;
+        }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", senderId=").append(senderId);
-        sb.append(", id=").append(id);
-        sb.append(", recipientId=").append(recipientId);
-        sb.append(", message=").append(message);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
+        public Builder recipientId(Integer val) {
+            recipientId = val;
+            return this;
+        }
+
+        public Builder message(String val) {
+            message = val;
+            return this;
+        }
+
+        public Builder createTime(LocalDateTime val) {
+            createTime = val;
+            return this;
+        }
+
+        public Message build() {
+            return new Message(this);
+        }
     }
 }
--- src/main/java/com/coconut/backend/entity/dto/Premium.java
+++ src/main/java/com/coconut/backend/entity/dto/Premium.java
@@ -1,11 +1,13 @@
 package com.coconut.backend.entity.dto;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -13,147 +15,60 @@ import java.util.Date;
 @NoArgsConstructor
 @TableName("premium")
 public class Premium implements Serializable {
-    /**
-     * 
-     */
     private Integer userId;
-
-    /**
-     * 
-     */
     private String vipType;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime lastTime;
 
-    /**
-     * 
-     */
-    private Date startTime;
-
-    /**
-     * 
-     */
-    private Date endTime;
-
-    /**
-     * 
-     */
-    private Date lastTime;
-
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * 
-     */
-    public Integer getUserId() {
-        return userId;
+    private Premium(Builder builder) {
+        this.userId = builder.userId;
+        this.vipType = builder.vipType;
+        this.startTime = builder.startTime;
+        this.endTime = builder.endTime;
+        this.lastTime = builder.lastTime;
     }
 
-    /**
-     * 
-     */
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
+    public static class Builder {
+        private Integer userId;
+        private String vipType;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime startTime;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime endTime;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime lastTime;
 
-    /**
-     * 
-     */
-    public String getVipType() {
-        return vipType;
-    }
-
-    /**
-     * 
-     */
-    public void setVipType(String vipType) {
-        this.vipType = vipType;
-    }
-
-    /**
-     * 
-     */
-    public Date getStartTime() {
-        return startTime;
-    }
-
-    /**
-     * 
-     */
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
-
-    /**
-     * 
-     */
-    public Date getEndTime() {
-        return endTime;
-    }
-
-    /**
-     * 
-     */
-    public void setEndTime(Date endTime) {
-        this.endTime = endTime;
-    }
-
-    /**
-     * 
-     */
-    public Date getLastTime() {
-        return lastTime;
-    }
-
-    /**
-     * 
-     */
-    public void setLastTime(Date lastTime) {
-        this.lastTime = lastTime;
-    }
-
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
+        public Builder userId(Integer val) {
+            userId = val;
+            return this;
         }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        Premium other = (Premium) that;
-        return (this.getUserId() == null ? other.getUserId() == null : this.getUserId().equals(other.getUserId()))
-            && (this.getVipType() == null ? other.getVipType() == null : this.getVipType().equals(other.getVipType()))
-            && (this.getStartTime() == null ? other.getStartTime() == null : this.getStartTime().equals(other.getStartTime()))
-            && (this.getEndTime() == null ? other.getEndTime() == null : this.getEndTime().equals(other.getEndTime()))
-            && (this.getLastTime() == null ? other.getLastTime() == null : this.getLastTime().equals(other.getLastTime()));
-    }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getUserId() == null) ? 0 : getUserId().hashCode());
-        result = prime * result + ((getVipType() == null) ? 0 : getVipType().hashCode());
-        result = prime * result + ((getStartTime() == null) ? 0 : getStartTime().hashCode());
-        result = prime * result + ((getEndTime() == null) ? 0 : getEndTime().hashCode());
-        result = prime * result + ((getLastTime() == null) ? 0 : getLastTime().hashCode());
-        return result;
-    }
+        public Builder vipType(String val) {
+            vipType = val;
+            return this;
+        }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", userId=").append(userId);
-        sb.append(", vipType=").append(vipType);
-        sb.append(", startTime=").append(startTime);
-        sb.append(", endTime=").append(endTime);
-        sb.append(", lastTime=").append(lastTime);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
+        public Builder startTime(LocalDateTime val) {
+            startTime = val;
+            return this;
+        }
+
+        public Builder endTime(LocalDateTime val) {
+            endTime = val;
+            return this;
+        }
+
+        public Builder lastTime(LocalDateTime val) {
+            lastTime = val;
+            return this;
+        }
+
+        public Premium build() {
+            return new Premium(this);
+        }
     }
 }
